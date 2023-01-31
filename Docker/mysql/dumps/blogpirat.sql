185e70f6dbba233a7185b2f6aafb91b3a39412f6
Dieter Koch <103990262+WebPirat@users.noreply.github.com>
2023-01-31 21:26:42 +0100

    update

--- Docker/mysql/dumps/blogpirat.sql
+++ Docker/mysql/dumps/blogpirat.sql
@@ -1,76 +1,180 @@
--- MySQL dump 10.13  Distrib 8.0.18, for Win64 (x86_64)
---
--- Host: localhost    Database: blogpirat
--- ------------------------------------------------------
--- Server version	8.0.32
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `sites`
---
+DROP TABLE IF EXISTS `blog_cat`;
+CREATE TABLE `blog_cat` (
+  `ID` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `img` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `deleted` int DEFAULT '1',
+  `created_At` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_At` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_At` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `id` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `blog_entrys`;
+CREATE TABLE `blog_entrys` (
+  `ID` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `title` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `teaser` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `online` int DEFAULT '0',
+  `site_alias` text,
+  `deleted` int DEFAULT '0',
+  `version` int DEFAULT '1',
+  `author` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `lastAuthor` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `created_At` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_At` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_At` datetime DEFAULT NULL,
+  `content` longtext,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `id` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `blog_entrys_backup`;
+CREATE TABLE `blog_entrys_backup` (
+  `ID` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `oldID` bigint unsigned NOT NULL,
+  `title` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `teaser` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `content` longtext CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `online` int DEFAULT '0',
+  `site_alias` text,
+  `deleted` int DEFAULT '0',
+  `version` int DEFAULT '1',
+  `author` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `lastAuthor` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `created_At` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_At` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_At` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `id` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `settings`;
+CREATE TABLE `settings` (
+  `ID` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `content` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `cat` bigint unsigned DEFAULT NULL,
+  UNIQUE KEY `id` (`ID`),
+  KEY `group` (`cat`),
+  CONSTRAINT `settings_ibfk_1` FOREIGN KEY (`cat`) REFERENCES `settings` (`ID`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `settings_cat`;
+CREATE TABLE `settings_cat` (
+  `ID` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` text,
+  UNIQUE KEY `id` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 
 DROP TABLE IF EXISTS `sites`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `sites` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `top_id` int DEFAULT '0',
+  `ID` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `topID` int DEFAULT '0',
   `name` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `online` int DEFAULT '0',
   `site_alias` text,
   `deleted` int DEFAULT '0',
-  `version` int DEFAULT '0',
+  `version` int DEFAULT '1',
   `author` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `lastAuthor` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `sort_order` int DEFAULT '0',
   `created_At` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_At` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `deleted_At` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `id` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `sites_backup`;
+CREATE TABLE `sites_backup` (
+  `ID` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `oldID` bigint unsigned DEFAULT NULL,
+  `topID` int DEFAULT '0',
+  `name` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `online` int DEFAULT '0',
+  `site_alias` text,
+  `deleted` int DEFAULT '0',
+  `version` int DEFAULT '1',
+  `author` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `sort_order` int NOT NULL,
+  `lastAuthor` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `created_At` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_At` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_At` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `id` (`ID`),
+  KEY `oldID` (`oldID`),
+  CONSTRAINT `sites_backup_ibfk_1` FOREIGN KEY (`oldID`) REFERENCES `sites` (`ID`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3;
+
+DROP TABLE IF EXISTS `sites_meta`;
+CREATE TABLE `sites_meta` (
+  `ID` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `sitesID` bigint unsigned DEFAULT NULL,
+  `title` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `img` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `keywords` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `version` int DEFAULT '1',
+  `created_At` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_At` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `id` (`ID`),
+  KEY `sitesID` (`sitesID`),
+  CONSTRAINT `sites_meta_ibfk_1` FOREIGN KEY (`sitesID`) REFERENCES `sites` (`ID`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `sites`
---
+DROP TABLE IF EXISTS `sites_meta_backup`;
+CREATE TABLE `sites_meta_backup` (
+  `ID` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `oldID` bigint unsigned DEFAULT NULL,
+  `sitesID` bigint unsigned DEFAULT NULL,
+  `title` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `description` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `img` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `keywords` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `version` int DEFAULT '1',
+  `created_At` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_At` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `id` (`ID`),
+  KEY `sitesID` (`sitesID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
-LOCK TABLES `sites` WRITE;
-/*!40000 ALTER TABLE `sites` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sites` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = '' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`blogpirat`@`%`*/ /*!50003 TRIGGER `deletedAtupdate` BEFORE UPDATE ON `sites` FOR EACH ROW BEGIN
-    IF (NEW.deleted != 0) THEN
-        SET NEW.deleted_At = current_timestamp();
-    END IF;
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
---
--- Dumping routines for database 'blogpirat'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+
+
+
+
+INSERT INTO `settings` (`ID`, `name`, `content`, `cat`) VALUES
+(1, 'title', 'Blog Pirat', 1);
+
+
+INSERT INTO `settings_cat` (`ID`, `name`) VALUES
+(1, 'meta');
+
+
+
+
+
+
+
+
+
+
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -79,5 +183,3 @@ DELIMITER ;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-01-25 23:48:59
--- web/classes/router.php
+++ web/classes/router.php
@@ -33,14 +33,26 @@ class router
         $title = $this->settings->getOneSetting('title', 'meta');
         echo '<!DOCTYPE html>
                 <html>
-                <head>
+                <head>                
                   <title>'.$title.'</title>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/assets/icon/apple-touch-icon.png">
+                    <link rel="icon" type="image/png" sizes="32x32" href="/assets/icon/favicon-32x32.png">
+                    <link rel="icon" type="image/png" sizes="16x16" href="/assets/icon/favicon-16x16.png">
+                    <link rel="manifest" href="/assets/icon/site.webmanifest">
+                    <link rel="mask-icon" href="/assets/icon/safari-pinned-tab.svg" color="#5bbad5">
+                    <link rel="shortcut icon" href="/assets/icon/favicon.ico">
+                    <link rel="stylesheet" href="/assets/css/blogpirat.css">
+                    <meta name="msapplication-TileColor" content="#da532c">
+                    <meta name="msapplication-config" content="/assets/icon/browserconfig.xml">
+                    <meta name="theme-color" content="#ffffff">
                 </head>';
     }
     public function getContent(){
-        echo 'Body';
+        echo '<body class="container">';
+        echo 'Yolo';
+        echo '</body>';
     }
     public function getFooter(){
-        echo 'Footer';
+        echo '</html>';
     }
 }
